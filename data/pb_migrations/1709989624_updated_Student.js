86238d2e5f77e63d7737ed6112bf2a4494c07d6d
mskim <msung98@naver.com>
2024-04-29 12:20:30 +0900

    Docker setting complete
    1.Backend 폴더 삭제
    2. Frontend 폴더에 Dockerfile 셋팅
    3. 루트 디렉토리에 Docker-compose셋팅
    4. data폴더 생성 후 pocketbase 데이터 이관
    5. gitignore 추가

---
+++ data/pb_migrations/1709989624_updated_Student.js
@@ -0,0 +1,69 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5xkv4hm6",
+    "name": "s_email",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "d0pbilna",
+    "name": "school",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "eoawzv58",
+    "name": "major",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("5xkv4hm6")
+
+  // remove
+  collection.schema.removeField("d0pbilna")
+
+  // remove
+  collection.schema.removeField("eoawzv58")
+
+  return dao.saveCollection(collection)
+})
