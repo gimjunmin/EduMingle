a5668264347a54d462e666f5a2bb882e60aabb3f
mskim <msung98@naver.com>
2024-06-03 22:30:13 +0900

    컨설턴트 프로필 1차
    상단 완성

--- Frontend/src/app/profile/page.tsx
+++ Frontend/src/app/profile/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import ConCard from "@/components/ConCard";
+import { useState } from "react";
+
+// components/ProfileCard.js
 export default function Profile() {
-	return <div>Profile page</div>;
+	let [select, setSelect] = useState(0);
+	return (
+		<div>
+			<div className="bg-sub3 h-32 w-full" />
+			<div className="ml-32">
+				<ConCard />
+			</div>
+		</div>
+	);
 }
